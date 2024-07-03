376f6429750a0353e88130f5ac50f2b847523cc0
DESKTOP-2GUM7RH\TM <mxolisizeal@gmail.com>
2024-07-03 20:10:18 +0200

    feat: updates the base openjdk base image and updates the post endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:latest
+FROM openjdk:23-jdk-oracle
 ARG JAR_FILE=build/libs/*.jar
 COPY /build/libs/propulsion-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/propulsion/app/propulsion/Controllers/ApiController.java
+++ src/main/java/com/propulsion/app/propulsion/Controllers/ApiController.java
@@ -1,18 +1,25 @@
 package com.propulsion.app.propulsion.Controllers;
 
+import com.propulsion.app.propulsion.Models.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
     @GetMapping(value = "/")
-    public String getPage() {
+    public String getPage()
+    {
         return "Welcome";
     }
 
-    @GetMapping(value = "/post")
-    public String getAnotherPage() {
-        return "Another Welcome";
+    @PostMapping(value = "/getUserID")
+    public User getUserID(@RequestBody String userId)
+    {
+        User user = new User();
+        user.setId(Integer.parseInt(userId));
+        return user;
     }
 }
